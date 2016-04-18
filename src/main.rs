de4f36d7b43bc54ae485c38392b236b30fee2296
Francisco Gómez <espectalll@kydara.com>
2016-04-18 13:14:52 +0200

    Doing even more stuff!

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 extern crate core;
+extern crate libc;
 extern crate rand;
 extern crate gtk;
 
 use std::cell::RefCell;
+use std::ffi::CString;
+use libc::system;
 use rand::distributions::{IndependentSample, Range};
 use gtk::prelude::*;
-use gtk::{Builder, Window, Label, Button, ListBox};
+use gtk::{
+    Builder, Window, Label, Button, Entry,
+    ListStore, CellRendererText, TreeView, TreeViewColumn };
 
 macro_rules! clone {
     (@param _) => ( _ );
@@ -29,7 +34,8 @@ fn main() {
         println!("Ayy lkek y u no Gtk+ m8");
     }
     
-    let used: Vec<i32> = Vec::new();
+    let used: Vec<i32> = Vec::new(); /* Should consider implementing
+                                        my own GtkTreeModel class instead */
     let between = Range::new(1, 91);
     
     let used_h = RefCell::new(used);
@@ -38,14 +44,23 @@ fn main() {
     let window: Window = builder.get_object("AppWindow").unwrap();
     let button: Button = builder.get_object("NextButton").unwrap();
     let label: Label = builder.get_object("GuessedLabel").unwrap();
-    let list_box: ListBox = builder.get_object("UsedListBox").unwrap();
+    let list: ListStore = builder.get_object("UsedListStore").unwrap();
+    let list_view: TreeView = builder.get_object("UsedListView").unwrap();
+    let search: Entry = builder.get_object("SearchEntry").unwrap();
+    let column = TreeViewColumn::new();
+    let cell_r = CellRendererText::new();
+
+    column.pack_start(&cell_r, true);
+    column.add_attribute(&cell_r, "text", 0);
+    list_view.append_column(&column);
+    list_view.set_search_entry(Some(&search));
     
     window.connect_delete_event(|_, _| {
         gtk::main_quit();
         Inhibit(false)
     });
     button.connect_clicked(
-        clone!(used_h, between, label, button => move |_| {
+        clone!(used_h, between, label, button, list => move |_| {
         
         let mut guessed: i32;
         let mut used = used_h.borrow_mut();
@@ -59,13 +74,22 @@ fn main() {
         
             used.push(guessed);
             used.sort();
-        
+            
+            list.insert_with_values(
+                None, &[0], &[&ToValue::to_value(&guessed.to_string())]);
+            
             label.set_label(&guessed.to_string());
+            
+            /* This is obviously a temporal, crappy patch
+               Must be replaced with Festival via C API */
+            unsafe {
+                let cmd = CString::new(
+                    format!("espeak -v spanish {}", guessed.to_string()))
+                    .unwrap().as_ptr();
+                system(cmd);
+            }
          }
          if used.len() == 90 {
-            /* for i in used {
-                println!("{}", i);
-            } */
             button.set_sensitive(false);
          }
     }));
